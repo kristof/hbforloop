9240a93a24e0afd3241de5b1ed30c1c5b33fca23
Kristof Houben <kristof@treeshadow.be>
2016-05-04 10:33:45 +0200

    fix limit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports.register = function (Handlebars, options)  {
   Handlebars.registerHelper('for', function(from, to, incr, block) {
       var accum = '';
-      for(var i = from; i < to; i += incr)
+      for(var i = from; i <= to; i += incr)
           accum += block.fn(i);
       return accum;
   });
